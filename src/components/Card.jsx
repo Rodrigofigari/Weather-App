135cd8feb792dbf34545670a0a9dba7ce19dba2c
rodrigo <rodrigojfigari@gmail.com>
2023-03-21 19:01:12 -0300

    https en el icono

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -36,7 +36,7 @@ export default function Card({ min, max, name, img, onClose, id }) {
           <div className="col-sm-4 col-md-4 col-lg-4">
             <img
               className="iconoClima"
-              src={"http://openweathermap.org/img/wn/" + img + "@2x.png"}
+              src={"https://openweathermap.org/img/wn/" + img + "@2x.png"}
               width="80"
               height="80"
               alt=""
